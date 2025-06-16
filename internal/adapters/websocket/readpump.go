56dfe5ded77158d8facb9b2c20467eaa562d2d76
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2025-06-16 19:12:16 +0200

    func/handle de mensajes en readpump: implementación de controlador para manejar tipos de mensajes.

--- internal/adapters/websocket/readpump.go
+++ internal/adapters/websocket/readpump.go
@@ -1,6 +1,11 @@
 package websocket
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/jorgerr9011/cartas-game-backend/internal/domain/player"
+)
 
 func ReadPump(rm *RoomManager, client *Client) {
 	defer func() {
@@ -15,12 +20,48 @@ func ReadPump(rm *RoomManager, client *Client) {
 			break
 		}
 
-		log.Printf("SALA DE JUEGO: %s", client.RoomID)
-		log.Printf("OBJECT SALA: %#v", rm.DomainRooms[client.RoomID])
-
-		rm.Broadcast <- Message{
-			RoomID:  client.RoomID,
-			Content: message,
+		var jsonMsg Message
+		if err := json.Unmarshal(message, &jsonMsg); err != nil {
+			log.Printf("Error deserializando mensaje %s: %v", client.ID, err)
+			return
 		}
+
+		if jsonMsg.RoomID == "" {
+			jsonMsg.RoomID = client.RoomID // fallback si no se incluye el RoomID explícitamente
+		}
+
+		handleMessage(rm, client, jsonMsg)
+
+		// rm.Broadcast <- Message{
+		// 	Type:    jsonMsg.Type,
+		// 	RoomID:  jsonMsg.RoomID,
+		// 	Payload: jsonMsg.Payload,
+		// }
+	}
+}
+
+func handleMessage(rm *RoomManager, client *Client, msg Message) {
+	switch msg.Type {
+	case "start_game":
+		// comienzo de juego
+		room := rm.DomainRooms[msg.RoomID]
+		room.Game.Start(room.Players)
+		state := room.Game.GetState()
+		log.Printf("Estado al empezar el juego: %#v", state)
+
+		hand, err := room.Game.GetPlayerHand(player.PlayerID(client.Username + "-id"))
+		if err != nil {
+			log.Printf("Error obteniendo mano del jugador: %v", err)
+		}
+		log.Printf("Mano del jugador %v: %#v", client.Username, hand)
+
+	case "player_move":
+		//
+
+	case "chat":
+		//
+
+	default:
+		log.Printf("Tipo de mensaje desconocido: %s", msg.Type)
 	}
 }
